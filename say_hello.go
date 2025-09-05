8fa6e4395b2ba90ac02cf6b11ce6c0dc2b10704a
samsudin <mohamadsamsudin47@gmail.com>
2025-09-05 14:07:22 +0700

    fix:sayhello function

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,10 @@
 package say_hello
 
 func SayHello() string {
-	return "Hello, World!"
+	return "Hello, Samsudin! , Welcome to Golang"
 }
+
+// catatan
+// cara membuat module ke github -> git tag v1.0.0
+
+// menambah dependensi
